eed7570f17cb01f7117c63a74aa80b8eb9a1a276
Don <don.j.olmstead@gmail.com>
2021-02-02 21:12:53 -0800

    Update include-what-you-use to v0.15.0

--- ports/include-what-you-use/portfile.cmake
+++ ports/include-what-you-use/portfile.cmake
@@ -1,12 +1,12 @@
 include(vcpkg_common_functions)
 
-set(VERSION 0.14)
+set(VERSION 0.15)
 
 # Get archive
 vcpkg_download_distfile(ARCHIVE
     URLS "https://include-what-you-use.org/downloads/include-what-you-use-${VERSION}.src.tar.gz"
     FILENAME "include-what-you-use-${VERSION}.src.tar.gz"
-    SHA512 ac328c6bdf834fde58bbf14c662fdf97aac22a24a85bce4a6475c73027b3ec1c558925efb5f0ac3addf446f21d29e37a70a7c5773c178f9ab7f12e90be7d69b4
+    SHA512 17e368c55103f956c91e6ab7c53c3a4528a21ea352f8217f61d2c741e635707b59c7bf7fec4ed25065f1ed8ec3073eca391123c715fbf624c2c722e61c334534
 )
 
 # Extract archive
@@ -14,6 +14,7 @@ vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
     ARCHIVE ${ARCHIVE}
     REF ${VERSION}
+    NO_REMOVE_ONE_LEVEL
 )
 
 # Run CMake build
@@ -34,4 +35,5 @@ file(RENAME ${CURRENT_PACKAGES_DIR}/bin/fix_includes.py ${CURRENT_PACKAGES_DIR}/
 file(RENAME ${CURRENT_PACKAGES_DIR}/bin/include-what-you-use.exe ${CURRENT_PACKAGES_DIR}/tools/include-what-you-use/include-what-you-use.exe)
 file(RENAME ${CURRENT_PACKAGES_DIR}/bin/iwyu_tool.py ${CURRENT_PACKAGES_DIR}/tools/include-what-you-use/iwyu_tool.py)
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
 file(INSTALL ${SOURCE_PATH}/LICENSE.TXT DESTINATION ${CURRENT_PACKAGES_DIR}/share/include-what-you-use RENAME copyright)
